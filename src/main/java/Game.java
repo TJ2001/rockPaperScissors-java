965978ed1fb9e6f082b667ee60162e55f40b1bf5
Eric Krause and Tim Jung <eric+tim@>
2016-09-07 14:28:29 -0700

    add more tests and get results

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -3,9 +3,30 @@ import java.util.List;
 
 public class Game {
 
-  public String returnWinner(String rockPaperScissors){
-    String result = "player 2";
-    return result;
+  public String pickWinner(String playerOne, String playerTwo){
+    String winner = "";
+    String p2 = "playerTwo wins";
+    String p1 = "playerOne wins";
+    String tie = "Tie";
+    if(playerOne.equals("rock") && playerTwo.equals( "paper")){
+      return winner = p2;
+    } else if(playerOne.equals("paper") && playerTwo.equals("scissors")){
+      return winner = p2;
+    } else if(playerOne.equals("scissors") && playerTwo.equals("rock")) {
+      return winner = p2;
+    } else if(playerOne.equals("rock") && playerTwo.equals("scissors")) {
+      return winner = p1;
+    } else if(playerOne.equals("scissor") && playerTwo.equals("rock")) {
+      return winner = p1;
+    } else if(playerOne.equals("scissor") && playerTwo.equals("paper")) {
+      return winner = p1;
+    } else if(playerOne.equals("rock") && playerTwo.equals("rock")) {
+      return winner = tie;
+    } else if(playerOne.equals("scissor") && playerTwo.equals("scissor")) {
+      return winner = tie;
+    } else if(playerOne.equals("paper") && playerTwo.equals("paper")) {
+      return winner = tie;
+    }
+        return(winner);
   }
-
 }
--- src/test/java/RockPaperScissorsTest.java
+++ src/test/java/RockPaperScissorsTest.java
@@ -2,31 +2,62 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class RockPaperScissorsTest {
+  Game testGame = new Game();
+
   @Test
-  public void pickWinner_PaperBeatsRock_Player1() {
-    Rock rocky = new Rock();
-    Paper mrPaper = new Paper();
-
-    assertTrue(mrPaper.beats(rocky)>0);
-    assertFalse(rocky.beats(mrPaper)>0);
-
+  public void pickWinner_PaperBeatsRock_PlayerTwo() {
+      String expected = "playerTwo wins";
+      String playerOne = "rock";
+      String playerTwo = "paper";
+      assertEquals(expected, testGame.pickWinner(playerOne, playerTwo));
+    }
+  @Test
+  public void pickWinner_ScissorBeatsPaper_PlayerTwo() {
+    String expected = "playerTwo wins";
+    String playerOne = "paper";
+    String playerTwo = "scissors";
+    assertEquals(expected, testGame.pickWinner(playerOne, playerTwo));
+    }
+  @Test
+  public void pickWinner_RockBeatsScissor_PlayerTwo() {
+    String expected = "playerTwo wins";
+    String playerOne = "scissors";
+    String playerTwo = "rock";
+    assertEquals(expected, testGame.pickWinner(playerOne, playerTwo));
+    }
+  @Test
+  public void pickWinner_RockBeatsSissor_PlayerOne(){
+    String expected = "playerOne wins";
+    String playerOne = "rock";
+    String playerTwo = "scissors";
+    assertEquals(expected, testGame.pickWinner(playerOne, playerTwo));
   }
-
   @Test
-  public void pickWinner_RockBeatsScissors_1(){
-    Rock rocky = new Rock();
-    Scissors sirScissor = new Scissors();
-
-    assertTrue(rocky.beats(sirScissor)>0);
-    assertFalse(sirScissor.beats(rocky)>0);
+  public void pickWinner_ScissorBeatsPaper_PlayerOne(){
+    String expected = "playerOne wins";
+    String playerOne = "scissor";
+    String playerTwo = "rock";
+    assertEquals(expected, testGame.pickWinner(playerOne,playerTwo));
   }
-
   @Test
-  public void pickWinner_RockTiesRock(){
-    Rock rocky = new Rock();
-    Rock mrRock = new Rock();
-
-    assertTrue(rocky.beats(mrRock)==0);
-    assertTrue(mrRock.beats(rocky)==0);
+  public void pickWinner_RockTiesRock_Tie(){
+    String expected = "Tie";
+    String playerOne = "rock";
+    String playerTwo = "rock";
+    assertEquals(expected, testGame.pickWinner(playerOne, playerTwo));
+  }
+  @Test
+  public void pickWinner_ScissorTiesScissor_Tie(){
+    String expected = "Tie";
+    String playerOne = "scissor";
+    String playerTwo = "scissor";
+    assertEquals(expected, testGame.pickWinner(playerOne, playerTwo));
+  }
+  @Test
+  public void pickWinner_PaperTiesPaper_Tie(){
+    String expected = "Tie";
+    String playerOne = "paper";
+    String playerTwo = "paper";
+    assertEquals(expected, testGame.pickWinner(playerOne, playerTwo));
   }
 }
